6e14dbd16392d81727f2c2183bcd8a8274f2355d
Che4891 <antoon.izotov@yandex.ru>
2025-09-08 20:32:24 +0300

    added test version

--- js/main.js
+++ js/main.js
@@ -1,3 +1,46 @@
+const output = document.getElementById("output");
+const log = (msg) => output.textContent += msg + "\n";
+
+document.getElementById("register").addEventListener("click", async () => {
+  try {
+    const publicKey = {
+      challenge: new Uint8Array(32), 
+      rp: { name: "Demo PWA" },
+      user: {
+        id: new Uint8Array([1,2,3,4]), 
+        name: "demo@example.com",
+        displayName: "Demo User"
+      },
+      pubKeyCredParams: [{ alg: -7, type: "public-key" }], 
+      authenticatorSelection: { userVerification: "preferred" },
+      timeout: 60000,
+      attestation: "direct"
+    };
+
+    const credential = await navigator.credentials.create({ publicKey });
+    log("Passkey success");
+    log(JSON.stringify(credential, null, 2));
+  } catch (err) {
+    log("Error" + err);
+  }
+});
+
+document.getElementById("login").addEventListener("click", async () => {
+  try {
+    const publicKey = {
+      challenge: new Uint8Array(32), 
+      timeout: 60000,
+      userVerification: "preferred"
+    };
+
+    const assertion = await navigator.credentials.get({ publicKey });
+    log("Login success");
+    log(JSON.stringify(assertion, null, 2));
+  } catch (err) {
+    log("Login error", err);
+  }
+});
+
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
       .register("/sw.js")
